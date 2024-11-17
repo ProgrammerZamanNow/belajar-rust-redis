652dfd18ce0343f474627e70acaa3812b1324a4a
khannedy <echo.khannedy@gmail.com>
2024-11-17 21:35:56 +0700

    Add unit test for Redis string expiration
    This commit introduces a new unit test to validate the expiration feature of Redis strings. The added test sets a string key with an expiration time, verifies its value, waits for the expiration duration, and then checks that the key has indeed expired.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,24 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
     use redis::{AsyncCommands, Client, Commands, RedisError};
     use redis::aio::MultiplexedConnection;
+
+    #[tokio::test]
+    async fn test_string() -> Result<(), RedisError> {
+        let mut con = get_client().await?;
+        let _ : () = con.set_ex("name", "Kurniawan", 2).await?;
+        let value: String = con.get("name").await?;
+        println!("{}", value);
+
+        tokio::time::sleep(Duration::from_secs(5)).await;
+
+        let value: Result<String, RedisError> = con.get("name").await;
+        assert_eq!(true, value.is_err());
+
+        Ok(())
+    }
 
     #[tokio::test]
     async fn test_async_connection() -> Result<(), RedisError> {
