26129443ee0231f25dc9f16e4aa0b2b78bae3708
khannedy <echo.khannedy@gmail.com>
2024-11-17 19:01:41 +0700

    Add unit tests for Redis connection in main.rs
    Introduce both asynchronous and synchronous tests to verify Redis connections. These tests ensure data can be set and retrieved correctly using different methods.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use redis::{AsyncCommands, Client, Commands, RedisError};
+    use redis::aio::MultiplexedConnection;
+
+    #[tokio::test]
+    async fn test_async_connection() -> Result<(), RedisError> {
+        let mut con = get_client().await?;
+        let _ : () = con.set("name", "Kurniawan").await?;
+        let value: String = con.get("name").await?;
+
+        println!("{}", value);
+
+        Ok(())
+    }
+
+    async fn get_client() -> Result<MultiplexedConnection, RedisError> {
+        let client = Client::open("redis://localhost:6379/")?;
+        client.get_multiplexed_async_connection().await
+    }
+
+    #[test]
+    fn test_connection() {
+        let mut client = Client::open("redis://localhost:6379/").unwrap();
+
+        let _: () = client.set("name", "Eko").unwrap();
+        let value: String = client.get("name").unwrap();
+
+        println!("{}", value);
+    }
+}
